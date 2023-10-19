3fbabcb15d2f82ddff0194422193efab7dcefccd
David Poehlman <dpoehlman@arizona.edu>
2023-10-19 18:32:00 -0500

    Added method to exit program gracefully at the end of calculation.

--- insulin.c
+++ insulin.c
@@ -46,43 +46,56 @@ int main(void)
 {
     // Local Declarations:
     float glucoseTarget, glucoseCurrent, glucoseFinal, insulinAmt;
+    char ch;
 
     // Statements:
     printf("This script will Identify the amount of insulin you should take for your adjusted dose...\n");
+    printf("Please press enter to continue, or input 'q' or 'Q' to quit the program.\n\n");
 
-    // This is the main function that will call the inputChecker function to validate the inputs.
-    glucoseCurrent = inputChecker("Please start by entering in your current glucose level: ");
-    glucoseTarget  = inputChecker("Now enter the level your glucose is supposed to be at: ");
 
-    // This is the main function that will calculate the amount of insulin needed to take for an adjustive dose to compensate for a patient's high blood sugar level.
-	glucoseFinal = glucoseCurrent - glucoseTarget;
-	insulinAmt   = glucoseFinal / (float)CARBOHYDRATE_COVERAGE;
-
-    // This is the main function that will print the results of the calculations.
-
-    if (glucoseCurrent <= GLUCOSE_LOW)
+    while (1)
     {
-        printf("\n\nYour glucose level is much too low for an adjustive dose.\n");
-        printf("Please call your endocronologist for further assistance!\n");
-        return 0;
-    }
-    else if (glucoseCurrent >= GLUCOSE_HIGH)
-    {
-        printf("\n\nYour glucose level is much too high. Please call your endocronologist for further assistance!\n");
-        printf("If you are directed to take an adjustive dose based on the provided formula, please take\n");
-        printf("%.1f unit(s) of insulin to compensate.\n", insulinAmt);
-        return 0;
-    }
-    else if (insulinAmt < 0)
-	{
-        printf("\n\nAfter careful consideration... You dont need an adjustive dose of insulin!\n");
-        return 0;
-	}
-    else
-    {
-        printf("\nAfter careful consideration... You need to take more insulin.\n");
-	    printf("Please take %.1f unit(s) of insulin to compensate.\n", insulinAmt);
-    }
+        // This statement will ask the user if they want to quit the program.
+        ch = getchar();
+        if (ch == 'q' || ch == 'Q')
+        {
+            printf("\n\nThank you for using this program!\n");
+            return 0;
+        }
+
+        // This is the main function that will call the inputChecker function to validate the inputs.
+        glucoseCurrent = inputChecker("Please start by entering in your current glucose level: ");
+        glucoseTarget  = inputChecker("Now enter the level your glucose is supposed to be at: ");
+
+        // This is the main function that will calculate the amount of insulin needed to take for an adjustive dose to compensate for a patient's high blood sugar level.
+        glucoseFinal = glucoseCurrent - glucoseTarget;
+        insulinAmt   = glucoseFinal / (float)CARBOHYDRATE_COVERAGE;
+
+        // This is the main function that will print the results of the calculations.
+
+        if (glucoseCurrent <= GLUCOSE_LOW)
+        {
+            printf("\n\nYour glucose level is much too low for an adjustive dose.\n");
+            printf("Please call your endocronologist for further assistance!\n");
+        }
+        else if (glucoseCurrent >= GLUCOSE_HIGH)
+        {
+            printf("\n\nYour glucose level is much too high. Please call your endocronologist for further assistance!\n");
+            printf("If you are directed to take an adjustive dose based on the provided formula, please take\n");
+            printf("%.1f unit(s) of insulin to compensate.\n", insulinAmt);
+        }
+        else if (insulinAmt < 0)
+        {
+            printf("\n\nAfter careful consideration... You dont need an adjustive dose of insulin!\n");
+        }
+        else
+        {
+            printf("\nAfter careful consideration... You need to take more insulin.\n");
+            printf("Please take %.1f unit(s) of insulin to compensate.\n", insulinAmt);
+        }
+
+        printf("Please press enter to continue, or input 'q' or 'Q' to quit the program.\n\n");
+    }    
 	
 	return 0;
 }
